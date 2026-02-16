d54ed8c8ffcf1168e712972d43c7ec132ace0f6a
YishayOssi <yishayossi10@gmail.com>
2026-02-16 20:46:34 +0200

    2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import { CHECKPOINT_STATUS, INITIAL_MESSAGES } from "./data.js";
 import { checkUserAndCreateToken, checkToken} from "./token.js";
 
 const app = express();
 const port = 3010;
+app.use(cors());
 app.use(express.json());
 
 
@@ -12,6 +14,7 @@ app.use("/", (req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
 
 
 app.post("/api/login", (req, res) => {
@@ -40,6 +43,9 @@ app.post("/api/messages", checkToken, (req, res) => {
     INITIAL_MESSAGES.push(req.body)
     res.send("new message...")
 });
+
+
+
 
 
 
